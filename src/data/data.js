6f909c536bfd071ecde4cd9b11c36f99b0182c41
hir01 <glazunovden@yandex.ru>
2023-09-07 17:23:55 +0300

    update

--- src/data/data.js
+++ src/data/data.js
@@ -18,6 +18,47 @@ const data = {
 			keywords: [],
 		},
 	},
+	account: {
+		menu: [
+			{
+				icon: 'basket-shopping',
+				name: 'Корзина заказов',
+				href: '#',
+			}, {
+				icon: 'basket-shopping',
+				name: 'Заказы',
+				href: '#',
+			}, {
+				icon: 'basket-shopping',
+				name: 'Счет-фактуры',
+				href: '#',
+			}, {
+				icon: 'basket-shopping',
+				name: 'Рекламации и Возвраты',
+				href: '#',
+			}, {
+				icon: 'basket-shopping',
+				name: 'Установка',
+				href: '#',
+			}, {
+				icon: 'basket-shopping',
+				name: 'Контактные лица',
+				href: '#',
+			}, {
+				icon: 'basket-shopping',
+				name: 'Адреса доставки',
+				href: '#',
+			}, {
+				icon: 'basket-shopping',
+				name: 'Перевозчики',
+				href: '#',
+			}, {
+				icon: 'basket-shopping',
+				name: 'Управление документооборотом',
+				href: '#',
+			},
+		],
+	},
 };
 
 export default data;
