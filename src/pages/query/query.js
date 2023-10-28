5d2a5bfb2fbcd73fdd53476fcb30e691a860949e
Hiiragi0831 <glazunovden@yandex.ru>
2023-10-28 20:45:33 +0300

    массовые запросы

--- src/pages/query/query.js
+++ src/pages/query/query.js
@@ -1,37 +1,70 @@
 import {Form} from '../../components/form/form';
+import {Notyf} from 'notyf';
+
+const queryBox = document.querySelector('[data-query]');
+const querysBox = document.querySelector('[data-querys]');
+let notyf = new Notyf();
+
+const deleteQuery = () => {
+	querysBox.querySelector('.form').querySelectorAll('.form__group').forEach((el) => {
+		el.querySelector('[name="delete"]').addEventListener('click', () => {
+			el.remove();
+		});
+	});
+};
+
+const formGroup = (data) => {
+	const clone = querysBox.querySelector('template').content.cloneNode(true);
+	clone.querySelector('input[name="brand"]').value = data.brand;
+	clone.querySelector('input[name="numparts"]').value = data.numparts;
+	clone.querySelector('input[name="count"]').value = data.count;
+
+	querysBox.querySelector('.form .form__main').append(clone);
+
+	deleteQuery();
+};
 
 const addQuery = () => {
-	const queryBox = document.querySelector('[data-query]');
-	const querysBox = document.querySelector('[data-querys]');
 	let object = {};
-
-	const formGroup = (data) => {
-		const clone = querysBox.querySelector('template').content.cloneNode(true);
-		clone.querySelector('input[name="brand"]').value = data.brand;
-		clone.querySelector('input[name="numparts"]').value = data.numparts;
-		clone.querySelector('input[name="count"]').value = data.count;
-
-		querysBox.querySelector('.form .form__main').append(clone);
-	};
+	let trigger = true;
 
 	queryBox.querySelector('.form').addEventListener('submit', (evt) => {
 		evt.preventDefault();
+		trigger = true;
+
 		new FormData(evt.target).forEach((value, key) => {
 			object[key] = value;
+
+			if (!value) {
+				trigger = false;
+				notyf.error('Не все поля заполнены');
+			}
 		});
 
-		formGroup(object);
+		if (trigger) {
+			formGroup(object);
+		}
 	});
 };
 
 const init = () => {
-	if (document.querySelector('[data-items-request]')) {
-		const forms = document.querySelectorAll('[data-items-request]');
+	if (document.querySelector('.query')) {
+		let data = [];
+		const form = new Form('POST');
 		addQuery();
 
-		forms.forEach((item) => {
-			const form = new Form('POST');
-			form.init('https://my.loaderpro.ru/Main/orders', item);
+		querysBox.querySelector('.form').querySelector('[type="submit"]').addEventListener('click', (evt) => {
+			evt.preventDefault();
+
+			querysBox.querySelector('.form').querySelectorAll('.form__group').forEach((el) => {
+				let object = {};
+				new FormData(el).forEach((value, key) => {
+					object[key] = value;
+				});
+				data.push(object);
+			});
+
+			form.sendData(data, 'https://my.loaderpro.ru/Main/orders');
 		});
 	}
 };
