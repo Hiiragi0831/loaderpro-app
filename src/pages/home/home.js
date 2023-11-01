cda56a4b88eb8ad838b8604a611b950f402ad1dc
Hiiragi0831 <glazunovden@yandex.ru>
2023-11-01 17:03:27 +0300

    fix

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,9 +1,12 @@
 import Swiper from 'swiper';
+import {Autoplay} from 'swiper/modules';
 
 const init = () => {
 	// eslint-disable-next-line
 	const sliderBrands = new Swiper('.home__brands .swiper', {
+		modules: [Autoplay],
 		slidesPerView: 8,
+		autoplay: true,
 	});
 };
 
