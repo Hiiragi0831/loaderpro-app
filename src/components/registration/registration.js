49ead3475cdca20c8fe1f100e951368d84c49571
Hiiragi0831 <glazunovden@yandex.ru>
2023-11-02 17:10:51 +0300

    fix

--- src/components/registration/registration.js
+++ src/components/registration/registration.js
@@ -4,7 +4,18 @@ const init = () => {
 		const form = document.querySelector('[data-registration-form]');
 		const sendData = new Form('POST');
 
-		sendData.init('https://my.loaderpro.ru/Main/create_user/', form);
+		form.addEventListener('submit', async (evt) => {
+			evt.preventDefault();
+
+			document.querySelectorAll('.form__checkbox').forEach((el) => {
+				if (el.querySelector('input').checked) {
+					const input = document.querySelector(`[name="${el.querySelector('input').dataset.name}"]`);
+					input.value = el.querySelector('input').value;
+				}
+			});
+
+			sendData.send('https://my.loaderpro.ru/Main/create_user/', form);
+		});
 	}
 };
 
