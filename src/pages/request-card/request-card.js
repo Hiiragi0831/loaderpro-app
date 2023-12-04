c846cbdbf60b8f33414f8982065723868ab9d21a
Hiiragi0831 <glazunovden@yandex.ru>
2023-12-04 12:25:33 +0300

    формулы

--- src/pages/request-card/request-card.js
+++ src/pages/request-card/request-card.js
@@ -1,4 +1,21 @@
 const requestCard = document.querySelector('.request-card');
+
+const marginCalc = () => {
+	const margin = requestCard.querySelector('[name="margin"]');
+	const marginpercent = requestCard.querySelector('[name="marginpercent"]');
+	const budget = requestCard.querySelector('[name="budget"]');
+	const purchase = requestCard.querySelector('[name="purchase"]');
+	const additionalaccounts = requestCard.querySelector('[name="additionalaccounts"]');
+	const delivery = requestCard.querySelector('[name="delivery"]');
+
+	margin.disabled = true;
+	marginpercent.disabled = true;
+
+	// бюджет - закупка - доп затраты - доставка
+	margin.value = budget.value - purchase.value - additionalaccounts.value - delivery.value;
+	// маржа / бюджет * 100
+	marginpercent.value = margin.value / budget.value * 100;
+};
 const init = () => {
 	if (!requestCard) {
 		return;
@@ -10,6 +27,8 @@ const init = () => {
 	select.querySelector('select').addEventListener('change', (evt) => {
 		select.dataset.color = evt.target.value;
 	});
+
+	marginCalc();
 };
 
 export default {
