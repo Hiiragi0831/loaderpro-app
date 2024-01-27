010840e95a25ef79144e321a06e62d42f7a22ac9
Hiiragi0831 <glazunovden@yandex.ru>
2024-01-27 17:55:33 +0300

    button

--- src/pages/query-history/query-history.js
+++ src/pages/query-history/query-history.js
@@ -6,6 +6,11 @@ const init = () => {
 		const sendData = new Form('POST');
 
 		sendData.init('https://my.loaderpro.ru/query_history/search_history', form, document.querySelector('.query__box-items'));
+
+		const more = document.querySelector('[data-more-query-h] [data-more]');
+		const moreForm = new Form('POST');
+
+		moreForm.init('https://my.loaderpro.ru/query_history/preview_next', more, document.querySelector('[data-more-query-h] .query__box'));
 	}
 };
 
