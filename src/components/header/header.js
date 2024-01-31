8b9801dda35bc37cac137a2d4f538965beaa03ae
hir01 <glazunovden@yandex.ru>
2024-01-31 15:57:05 +0300

    mobile header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,11 +1,22 @@
 import {Form} from '../form/form';
 
+const mobileMenu = () => {
+	const burger = document.querySelector('.header-mobile');
+
+	burger.addEventListener('click', (evt) => {
+		evt.preventDefault();
+		document.querySelector('.header-mobile').classList.toggle('is-active');
+	});
+};
+
 const init = () => {
 	if (document.querySelector('.header')) {
 		const form = document.querySelector('[data-header-request]');
 		const sendData = new Form('POST');
 
 		sendData.init('https://my.loaderpro.ru/Main/orders/', form, document.querySelector('main'));
+
+		mobileMenu();
 	}
 };
 
