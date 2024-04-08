a6ba7614d00a1aa13a98a1ab7c1f5f78a7e5cae2
Hiiragi0831 <glazunovden@yandex.ru>
2024-04-08 13:29:17 +0300

    fix

--- src/pages/list-orders/list-orders.js
+++ src/pages/list-orders/list-orders.js
@@ -8,6 +8,14 @@ const init = () => {
 	}
 
 	sendForm.init('https://my.loaderpro.ru/basket/basket_search', page.querySelector('[data-search]'), page.querySelector('.page__results'));
+
+	page.querySelector('.page__tab').querySelectorAll('.navtab__item').forEach((i, id) => {
+		i.addEventListener('click', (e) => {
+			e.preventDefault();
+			console.log(i, id);
+			sendForm.sendData(id, 'https://my.loaderpro.ru/list_orders/list_order_status_result', page.querySelector('.page__results'));
+		});
+	});
 };
 
 export default {
