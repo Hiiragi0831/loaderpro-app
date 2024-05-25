4bd89623b5e4e140c55fbea6cd095d09bc2cc85a
hir01 <glazunovden@yandex.ru>
2024-05-25 15:11:40 +0300

    fix

--- src/pages/query-history/query-history.js
+++ src/pages/query-history/query-history.js
@@ -2,15 +2,19 @@ import {Form} from '../../components/form/form';
 
 const init = () => {
 	if (document.querySelector('[data-query-history]')) {
-		const form = document.querySelector('[data-query-history]');
 		const sendData = new Form('POST');
 
-		sendData.init('https://my.loaderpro.ru/query_history/search_history', form, document.querySelector('.query__box-items'));
+		sendData.init(
+			'https://my.loaderpro.ru/query_history/search_history',
+			document.querySelector('[data-query-history]'),
+			document.querySelector('.query__box-items'),
+		);
 
-		const more = document.querySelector('[data-more-query-h] [data-more]');
-		const moreForm = new Form('POST');
-
-		moreForm.init('https://my.loaderpro.ru/query_history/preview_next', more, document.querySelector('[data-more-query-h] .query__box'));
+		// sendData.init(
+		// 	'https://my.loaderpro.ru/query_history/preview_next',
+		// 	document.querySelector('[data-more-query-h] [data-more]'),
+		// 	document.querySelector('[data-more-query-h] .query__box'),
+		// );
 	}
 };
 
